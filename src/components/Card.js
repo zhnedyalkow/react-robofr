6323a517cdacd0ce91cc72d8364192fe4d80f0f9
zh.nedyalkow <zh.nedyalkow@gmail.com>
2019-02-21 07:42:58 +0200

    Initial commit

---
+++ src/components/Card.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Card = ({ id, name, email}) => {
+// const Card = (props) => {
+  // const { name, id, email } = props;
+
+    return (
+      <div className='tc bg-light-green dib br3 pa3 ma2 grow shadow-5'>
+        <img alt='robots' src={`https://robohash.org/${id}?200x200`}/>
+        <div>
+          <h2>{name}</h2>
+          {/* {props.name} */}
+          <p>{email}</p>
+        </div>
+      </div>
+    )
+}
+
+export default Card;
---
+++ src/robots.js
@@ -0,0 +1,50 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Zhitomir',
+    username: 'zhtomir',
+    email: 'zh.nedyalkow@gmail.com'
+  },
+  {
+    id: 2,
+    name: 'Vasil',
+    username: 'vasko',
+    email: 'vasko@gmail.com'
+  },
+  {
+    id: 3,
+    name: 'Tania',
+    username: 'tania',
+    email: 'tania@gmail.com'
+  },
+  {
+    id: 4,
+    name: 'Vesela',
+    username: 'vesi',
+    email: 'vesi@gmail.com'
+  },
+  {
+    id: 5,
+    name: 'Georgi',
+    username: 'georgi',
+    email: 'georgi@gmail.com'
+  },
+  {
+    id: 6,
+    name: 'Doncho',
+    username: 'doncho',
+    email: 'doncho@gmail.com'
+  },
+  {
+    id: 7,
+    name: 'Dino',
+    username: 'dino',
+    email: 'dino@gmail.com'
+  },
+  {
+    id: 8,
+    name: 'Momcho',
+    username: 'momcho',
+    email: 'momcho@gmail.com'
+  },
+];
